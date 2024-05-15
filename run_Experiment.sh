4adb8d403676621f7bd29cc109cfcd4b9f8d6be0
GanyuWang <wang_ganyu@hotmail.com>
2024-05-15 14:11:23 -0400

    Update gitignore for new sh files.

--- run_Experiment.sh
+++ run_Experiment.sh
@@ -9,10 +9,10 @@ python ./run_glue_LLM_FL.py \
 --sample_size 20 --prompt_length 10 \
 --prompt_search_space 200 \
 --api_limit 8000 --ce_loss True \
---num_train_epochs 20 \
---FL_framework FedAvg --num_clients 10 --num_activated_clients 1 --num_client_local_step 1 --max_client_train_steps 8000 \
+--num_train_epochs 10 \
+--FL_framework FedAvg --num_clients 10 --num_activated_clients 5 --num_client_local_step 1 --max_client_train_steps 8000 \
 --prompt_tuning_method BDPL \
---log_file_name TempResult 
+--log_file_name ExperimentResult/FedAvg_BDPL_NC10_AC5_Epoch10
 
 # # GPT-based experiments
 # python ./run_glue_discrete_GPT.py \
